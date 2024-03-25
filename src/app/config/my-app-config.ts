87d0e60d681dc26d59c5f1208fd7dc4c62fb1f2d
Efehan YILDIZ <s1146227@student.hsleiden.nl>
2024-03-25 03:42:18 +0100

    okta v1

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+    oidc: {
+        clientId: '0oag0cn1ymDLqGFQq5d7',
+        issuer: 'https://dev-79985562.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
